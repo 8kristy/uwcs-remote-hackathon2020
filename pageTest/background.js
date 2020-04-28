30c0c8570cc1fbd53f7a6848b2bc668bb1dee032
Lydia Shepherd <zeldapleasant@gmail.com>
2020-04-28 12:22:55 +0100

    created new PageTest folder which fetches full page data whenever a new page is loaded. Test and browser files are now obselete.

---
+++ pageTest/background.js
@@ -0,0 +1,12 @@
+function doStuffWithDom(domContent) {
+    //here will be where we end up sending the content to the backend i think?
+    console.log('I received the following content:\n' + domContent);
+}
+console.log("in background");
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  console.log(changeInfo.status);
+  if (changeInfo.status == 'complete' && tab.active) {
+    console.log("in if statement");
+    chrome.tabs.sendMessage(tab.id, {text: 'report_back_full'}, doStuffWithDom);
+  }
+});
