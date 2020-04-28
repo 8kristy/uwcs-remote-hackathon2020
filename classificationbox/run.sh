afe2cb1b93afe1c03fc3d21f9f14aa62ce09af8e
me <8kristy@gmail.com>
2020-04-28 15:51:58 +0100

    Parser adds all raw articles into suitable JSON file

--- classificationbox/run.sh
+++ classificationbox/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# starts the classificationbox
+python3 dataparser.py
 
+# starts classificationbox
 # get the MB KEY here https://machinebox.io/account?utm_source=matblog-fakenews&utm_medium=matblog-fakenews&utm_campaign=matblog-fakenews&utm_term=matblog-fakenews&utm_content=matblog-fakenews
 
-docker run -p 8080:8080 -e MB_KEY="key" -d --name box machinebox/classificationbox & 
+docker run -p 8080:8080 -e MB_KEY="key"-d --name box machinebox/classificationbox & 
 
 # wait for the initial setup, i don't know if there's an option to detect when it's ready so this is the next best thing
 sleep 10
@@ -13,8 +14,7 @@ sleep 10
 curl -X POST -H "Content-Type: application/json" -d @model.json http://localhost:8080/classificationbox/models
 
 # teaches the model
-curl -X POST -H "Content-Type: application/json" -d @neutral.json http://localhost:8080/classificationbox/models/model1/teach-multi
-curl -X POST -H "Content-Type: application/json" -d @biased.json http://localhost:8080/classificationbox/models/model1/teach-multi
-curl -X POST -H "Content-Type: application/json" -d @satire.json http://localhost:8080/classificationbox/models/model1/teach-multi
+curl -X POST -H "Content-Type: application/json" -d @toTeach.json http://localhost:8080/classificationbox/models/model1/teach-multi
+
 
 
