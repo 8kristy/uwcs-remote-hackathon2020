8c95cf0d0f706ecf58b62cf15443c0618e8d6982
me <8kristy@gmail.com>
2020-05-03 15:07:43 +0100

    Deleted redundant files

--- pageTest/content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back_full') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].innerText); //this returns full page of stripped html, need to try and remove certain heading and list parts
-});
